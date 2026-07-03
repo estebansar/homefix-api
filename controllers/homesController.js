c3f10991938860e62baa36bf8953ff6b7a918da2
estebansar <sar17008@byui.edu>
2026-07-03 17:26:21 -0600

    Connect homes to MongoDB

--- controllers/homesController.js
+++ controllers/homesController.js
@@ -1,11 +1,24 @@
-// This gets all homes from the database
+const mongodb = require("../data/database")
+const ObjectId = require("mongodb").ObjectId
+
+// This gets all homes from the homes collection
 const getAllHomes = async (req, res) => {
-  res.send("Get all homes")
+  const result = await mongodb.getDb().collection("homes").find()
+  const homes = await result.toArray()
+
+  res.setHeader("Content-Type", "application/json")
+  res.status(200).json(homes)
 }
 
 // This gets one home by id
 const getSingleHome = async (req, res) => {
-  res.send("Get single home")
+  const homeId = new ObjectId(req.params.id)
+
+  const result = await mongodb.getDb().collection("homes").find({ _id: homeId })
+  const homes = await result.toArray()
+
+  res.setHeader("Content-Type", "application/json")
+  res.status(200).json(homes[0])
 }
 
 module.exports = {

67f93c39660c722ba3ebe05b20981078b9099974
estebansar <sar17008@byui.edu>
2026-07-04 16:58:10 -0600

    Add users routes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 const homesController = require("../controllers/homesController")
+const usersController = require("../controllers/usersController")
 
 // This is a test route to make sure my routes folder is connected
 router.get("/", (req, res) => {
@@ -14,5 +15,12 @@ router.get("/homes/:id", homesController.getSingleHome)
 router.post("/homes", homesController.createHome)
 router.put("/homes/:id", homesController.updateHome)
 router.delete("/homes/:id", homesController.deleteHome)
+
+// Users routes
+router.get("/users", usersController.getAllUsers)
+router.get("/users/:id", usersController.getSingleUser)
+router.post("/users", usersController.createUser)
+router.put("/users/:id", usersController.updateUser)
+router.delete("/users/:id", usersController.deleteUser)
 
 module.exports = router
